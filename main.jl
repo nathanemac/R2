350137ca2f87f9f54b3f2ab8ec04d43145b9c031
farhadrclass <31899325+farhadrclass@users.noreply.github.com>
2022-04-29 12:18:41 -0400

    Update main.jl

--- main.jl
+++ main.jl
@@ -9,7 +9,7 @@ using SolverCore
 include("R2_DNN.jl")
 
 # 2 define a NLP (Problem ) aka #Ros(x) = (1-x[1])^2 + (x[2]-x[1]^2)^2 
-nlp = ADNLPModel(x -> (1 - x[1])^2 + (x[2] - x[1]^2)^2, [-1,2.0])
+nlp = ADNLPModel(x -> (1 - x[1])^2 + (x[2] - x[1]^2)^2, [-1, 2.0])
 
 # Optional turn on/off the table 
 #verbose=true
@@ -17,5 +17,15 @@ nlp = ADNLPModel(x -> (1 - x[1])^2 + (x[2] - x[1]^2)^2, [-1,2.0])
 
 # 3 select a solver aka R2 and send the problem to it 
 
-stats=R2(nlp)
+stats = R2(
+    nlp,
+    10000,
+    0.3,
+    0.7,
+    1/2,
+    2.0,
+    1e-6,
+    1e-6,
+    false
+)
 print(stats)
