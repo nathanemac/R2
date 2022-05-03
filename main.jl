0011d808a88e00d87c0c3189d077ae70cf959d72
nathanemac <91251698+nathanemac@users.noreply.github.com>
2022-05-03 13:33:38 -0400

    v3

--- main.jl
+++ main.jl
@@ -8,8 +8,8 @@ using SolverCore
 
 include("R2_DNN.jl")
 
-# 2 define a NLP (Problem ) aka #Ros(x) = (1-x[1])^2 + (x[2]-x[1]^2)^2 
-nlp = ADNLPModel(x -> (1 - x[1])^2 + (x[2] - x[1]^2)^2, [-1, 2.0])
+# 2 define a NLP (Problem )
+nlp = ADNLPModel(x -> (1 - x[1])^2 + (x[2] - x[1]^2)^2, [10.0, -10.5])
 
 # Optional turn on/off the table 
 #verbose=true
