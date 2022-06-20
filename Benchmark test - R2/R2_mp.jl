d8e497b5cdb74f0f07e3a27a5a9455c9c56fd2fa
nathanemac <91251698+nathanemac@users.noreply.github.com>
2022-06-20 11:01:42 -0400

    mp

--- Benchmark test - R2/R2_mp.jl
+++ Benchmark test - R2/R2_mp.jl
@@ -1,131 +1,52 @@
-" 
-    Solves the optimization problem min(f(x)) with quadratic regularization
-    Input: (x::Vector, f::function to minimize, maxiterations::Int, η1,η2,γ1,γ2,ϵ_abs,ϵ_rel :: Float64, verbose::Bool )
-    Output: (GenericExecutionStats)
-"
-function R2_mp(
-            nlp;
-            σ_min = nothing,
-            maxiterations=1000,
-            η1 = 0.3,
-            η2 = 0.7,
-            γ1 = 1 / 2,
-            γ2 = 2.0,
-            ϵ_abs = 1e-6,
-            ϵ_rel = 1e-6,
-            verbose::Bool = false)
+export bmark_solvers
 
-    # Initializing the variables
-
-    x0 = copy(nlp.meta.x0)
-    x0 = convert.(Type,x0)
-
-    if σ_min === nothing
-        σ_min = eps(Type)
-    else 
-        σ_min = convert(Type,σ_min)
+function bmark_solvers(solver,problem_suit::Dict{Symbol, <:Any}, args...; kwargs...)
+    stats = Dict{Symbol, DataFrame}()
+    for (name, problems) in problem_suit
+        @debug "running" name
+        stats[name] = solve_problems(solver, problems, args ...; kwargs...)
     end
-
-    # Initialisation
-    iter=0
-    xk = copy(x0)
-
-    ρk = 0
-    ck = copy(xk)
-    fk = obj(nlp, xk)
-    gk = grad(nlp, xk)
-    norm_gk=norm(gk)
-    σk = 2^round(log2(norm(gk) + 1)) # The closest exact-computed power of 2 from gk
-
-    γ1 =  convert.(Type,γ1)
-    γ2 =  convert.(Type,γ2)
-    σk =  convert.(Type,σk)
-
-
-
-    # Stopping criterion: 
-    ϵ = ϵ_abs + ϵ_rel*norm_gk
-    optimal = norm_gk ≤ ϵ
-    tired = iter > maxiterations
-
-    if verbose
-        @info @sprintf "%5s  %9s  %7s  %7s " "iter" "f" "‖∇f‖" "σ"
-        infoline = @sprintf "%5d  %9.2e  %7.1e  %7.1e" iter fk norm_gk σk
-    end
-
-    start_time = time()
-    elapsed_time = 0.0
-
-    while !(optimal | tired)
-
-        sk = convert.(Type,-gk./σk)
-
-        if norm(sk) < eps(Float16)
-            @warn "Stop because the step is lower than machine precision"
-            status = :small_step
-            break
-        end 
-
-        ck = xk .+ sk
-        ΔTk = -gk' * sk
-        fck = obj(nlp, ck)
-        if fck == -Inf
-            status = :unbounded
-            break
-        end
-
-        ρk = (fk - fck) / ΔTk
-
-
-        # Recomputing if conditions on ρk not reached
-        if ρk >= η2
-            σk = max(σ_min, γ1 * σk)
-
-        elseif ρk < η1
-            σk = σk * γ2
-
-        end
-
-        # Acceptance of the new candidate
-        if ρk >= η1
-            xk = ck
-            fk = fck
-            gk = grad(nlp, xk)
-            norm_gk=norm(gk)
-        end
-
-
-        iter += 1
-        optimal = norm_gk ≤ ϵ
-        tired = iter > maxiterations
-
-        elapsed_time = time() - start_time
-
-        if verbose
-            #infoline *= @sprintf "  %8.1e  %7.1e" ρk elapsed_time
-            @info infoline
-            infoline = @sprintf "%5d  %9.2e  %7.1e  %7.1e" iter fk norm_gk σk
-        end
-
-        global status
-
-        status = if optimal
-            :first_order
-          elseif tired
-            :max_iter
-          else
-            :exception
-          end
-    end
-    
-    return GenericExecutionStats(
-            status,
-            nlp,
-            solution = xk,
-            objective = fk,
-            dual_feas = norm_gk,
-            elapsed_time = elapsed_time,
-            iter = iter,
-            )
+    return stats
 end
 
+using JSOSolvers
+using LinearAlgebra
+using Random
+using Printf
+using DataFrames
+using OptimizationProblems
+using NLPModels
+using ADNLPModels
+using OptimizationProblems.ADNLPProblems
+using SolverCore
+using SolverBenchmark
+using DoubleFloats
+using Quadmath
+using BFloat16s
+using Statistics
+using CSV
+using Plots
+using Profile
+using ProfileView
+using StochasticRounding
+
+
+include("R2.jl")
+include("R2_D_M.jl")
+
+problemsF32 = (eval(problem)(type = Val(Float32)) for problem ∈ setdiff(names(OptimizationProblems.ADNLPProblems), [:ADNLPProblems]))
+problemsF32sr = (eval(problem)(type = Val(Float32sr)) for problem ∈ setdiff(names(OptimizationProblems.ADNLPProblems), [:ADNLPProblems]))
+
+solver = Dict(
+            :R2 => 
+              nlp -> R2(
+                  nlp;
+                  maxiterations=250,
+                  η1 = 1e-4,
+                  η2 = 0.75,
+                  γ1 = 0.33,
+                  γ2 = 2.0,
+                  verbose = false)
+                )       
+
+statsF32 = bmark_solvers(solver, problemsF32,  skipif = prob -> (!unconstrained(prob) || get_nvar(prob) > 100 || get_nvar(prob) < 5))
